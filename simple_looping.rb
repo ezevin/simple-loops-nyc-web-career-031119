9db6f8d0f56442f6b0dda2af190fdafeaba9f357
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-08 21:37:30 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -54,7 +54,7 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 1..7
-  for counter
+  for welcome in counter
   puts phrase
 end
 end
